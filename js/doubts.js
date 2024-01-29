0fc65fb6c50ec26b1a3eaccb6b725610c18512d1
Poorvi <pjnayak2003@gmail.com>
2024-01-29 17:38:28 +0530

    setTimeout doubt cleared

--- js/doubts.js
+++ js/doubts.js
@@ -3,3 +3,13 @@
 
 // var b = 10;
 // var b = 100; --> only 1 'b' variable present in global space
+
+// function x()
+// {
+//     a=0
+//     setTimeout(function(){
+//         console.log(a);
+//     },3000)
+//     a++;
+// }
+// x();
